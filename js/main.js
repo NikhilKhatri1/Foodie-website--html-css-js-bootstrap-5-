29f06ca88cf7b004ffe1b88392e740e94f11a6f0
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-05-20 22:45:51 +0530

    files added

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// action navbar
+
+// anv hide
+
+//counter design
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5897, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7287, 3000);
+});
