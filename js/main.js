bea5c35c213a974594cb128eaef9f4145b49a139
NikhilKhatri1 <nikhilkhattri2@gmail.com>
2024-05-24 00:39:48 +0530

    files modifed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,30 @@
-// action navbar
+// Active navbar
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("scroll-on");
+    } else {
+        nav.classList.remove("scroll-on");
+    }
+};
 
-// anv hide
+// Navbar hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show");
+    });
+});
 
-//counter design
+// Counter design
 document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration) {
         let obj = document.getElementById(id),
             current = start,
             range = end - start,
             increment = end > start ? 1 : -1,
-            // Adjusted the step to slow down the timer further
-            step = Math.abs(Math.floor(duration / range)) * 100, // Quadrupling the step to slow down the timer further
+            step = Math.abs(Math.floor(duration / range)), // Adjusted step calculation
             timer = setInterval(() => {
                 current += increment;
                 obj.textContent = current;
